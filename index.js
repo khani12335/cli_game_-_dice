7d4d36d46f07ef45ccf29539fe1d393780a6bba1
dani_khan <159428334+khani12335@users.noreply.github.com>
2024-04-19 16:16:50 +0500

    first_commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+let answer = await inquirer.prompt({
+    name: "numberGuessdGame",
+    type: "number",
+    message: "please gussed a number 1_6..."
+});
+if (answer.numberGuessdGame === randomNumber) {
+    console.log("congratulation! You guess a write number...");
+}
+else {
+    console.log("You guess a wrong number...");
+}
